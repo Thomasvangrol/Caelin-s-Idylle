0fcef6141dc47149c432dd06e77b2d95127adb5a
Iko <iko.jdr@gmail.com>
2025-11-12 19:26:35 +0100

    Added       "@config": ["src/js/load-config.js"]  and config.d.ts

---
+++ src/types/config.d.ts
@@ -0,0 +1,12 @@
+declare module "@config" {
+    export const CONFIG: {
+      TITLE: string;
+      URL: string;
+      DESCRIPTION: string;
+      LANG: string;
+      AUTHOR: string;
+      CONTACT: boolean;
+      EMAIL: string;
+      THUMBS: boolean;
+    };
+  }
